fb43981c6bfba0b93b9a43247e2c5a02613c8f12
gcgarciab <gcgarciab@unal.edu.co>
2016-01-01 11:47:03 -0500

    Con las otras pantallas

--- manantial_aseo/app/controllers/sales.rb
+++ manantial_aseo/app/controllers/sales.rb
@@ -1,23 +1,9 @@
 ManantialAseo::App.controllers :sales do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  layout :sales
 
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
+  get :sales do
+    render :sales
+  end
 
 end
